b4d33aa6d4d2d7342f7a5ec681e799dbb428850c
benjmain <bc20ou@snpsteam.com>
2024-03-22 13:31:06 -0400

    fixed bugs and errors
    I fixed some bugs inside of the html code and remove the :mailto link because to tag no longer works. I also added the github logo beside the github link on the contacts & soical media

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-
 var item = document.querySelectorAll(".boxMain");
 var title = document.querySelectorAll(".boxTitle");
 
-console.log(item); 
+console.log(item);
 console.log(title);
 
 
 
-for(let i = 0; i < item.length; i++){
+for (let i = 0; i < item.length; i++) {
     item[i].classList.add(hidden);
-    title[i].addEventListener("click", function(event){
-        console.log("click"+event);
+    title[i].addEventListener("click", function(event) {
+        console.log("click" + event);
     });
 }
 
 console.log();
-
